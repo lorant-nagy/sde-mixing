070f19ded61f6716d34bb6c751d2fc2228d5f95f
Lóránt Nagy <lorantnagy@Lorants-MacBook-Pro.local>
2026-02-09 17:12:41 +0100

    dev

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ PARAMS = {
     
     # Radii to test
     # 'R_values': np.array([1.0, 2.0, 3.0, 4.0, 5.0]),
-    'R_values': np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0]),
+    'R_values': np.array([7.0, 11.0, 15.0, 20.0]),
     
     # Thresholds for mixing time
     'p0_KS': 0.05,
